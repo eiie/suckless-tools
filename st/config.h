e02b984ec922a0b231498093a10fe574e8b22418
eiie <eiie@eiie.fr>
2022-03-06 12:49:54 +0100

    change font size

--- st/config.h
+++ st/config.h
@@ -5,7 +5,6 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-/* static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true"; */
 static char *font = "Consolas-Regular:size=11:antialias=true:autohint=true";
 static int borderpx = 5;
 
